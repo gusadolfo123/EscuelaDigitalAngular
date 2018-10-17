4cb3c20d72b16b86953249c45a069e37a9a6e56f
zetman-PC <zetman-PC@DESKTOP-54SM6J2>
2018-10-16 20:05:03 -0500

    Se agrega jquery para funcionamiento de vista home sidebar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import * as $ from "jquery";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    $(document).ready(function() {
+      $("#sidebarCollapse").on("click", function() {
+        // agrego estilo container cuando se ocula el sidebar
+        if (
+          $("#sidebar").attr("class") == "" ||
+          $("#sidebar").attr("class") == undefined
+        )
+          $("#content").attr("class", "container");
+        else $("#content").attr("class", "");
 
+        $("#sidebar").toggleClass("active");
+      });
+    });
+  }
 }
